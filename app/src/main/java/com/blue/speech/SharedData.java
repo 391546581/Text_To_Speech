428f79836ab27c1eab009708c3417248c79b54b0
blue <391546581@qq.com>
2018-07-25 15:12:51 +0800

    Initial commit

---
+++ app/src/main/java/com/blue/speech/SharedData.java
@@ -0,0 +1,16 @@
+package com.blue.speech;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class SharedData {
+
+    //语速
+    public static float voice_speed=0.5f;
+    //音调
+    public static float voice_pitch=1.0f;
+    //
+    public static Map<String,Locale> languageList=new HashMap<String,Locale>();
+
+}
